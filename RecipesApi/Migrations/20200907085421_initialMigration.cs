3df61c9d5a6cd7d3cc97b57c855af12e3ac441c2
Nattie <usolnatalia@gmail.com>
2020-09-15 16:47:26 +1200

    Uploaded missing files

---
+++ RecipesApi/Migrations/20200907085421_initialMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipesApi.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipe",
+                columns: table => new
+                {
+                    RecipeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RecipeImageUrl = table.Column<string>(nullable: true),
+                    RecipeName = table.Column<string>(nullable: false),
+                    RecipeDifficulty = table.Column<string>(nullable: false),
+                    RecipyCost = table.Column<string>(nullable: true),
+                    RecipeIngredients = table.Column<string>(nullable: false),
+                    RecipeDescription = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipe", x => x.RecipeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipe");
+        }
+    }
+}
---
+++ RecipesApi/Migrations/20200913214755_AddedCommentingTable.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipesApi.Migrations
+{
+    public partial class AddedCommentingTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commenting",
+                columns: table => new
+                {
+                    CommentingId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: false),
+                    CommentText = table.Column<string>(nullable: false),
+                    RecipeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commenting", x => x.CommentingId);
+                    table.ForeignKey(
+                        name: "FK_Commenting_Recipe_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipe",
+                        principalColumn: "RecipeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commenting_RecipeId",
+                table: "Commenting",
+                column: "RecipeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commenting");
+        }
+    }
+}
